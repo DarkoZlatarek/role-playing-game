cb2912f1c96f3219ef685bb66ea87934cde966a5
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-01-06 21:45:10 +0000

    Add function message for story intro

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import time
 import sys
 
 
@@ -18,12 +19,43 @@ class Character:
         return f'rase: {self.rase} \n health: {self.health} \n strength: {self.strength} \n defense: {self.defense}'
 
 
+MSG = 'Many years ago, warriors, mages and dark elves '\
+    'lived in unity. They shared knowledge of their ancestors '\
+    'and the way of life between each other. Until one-day '\
+    'dark elves decided that warriors and mages are beneath '\
+    'them. Dark elves migrated away from the warriors and mages '\
+    'into the mountains. For many years, they stick for '\
+    'themselves, but their resources were running low so they '\
+    'started raiding villages of warriors and mages. '\
+    'Stealing their food, women and man for slaves. '\
+    'One day, they have crossed the line.....'
+
+
 dict_of_characters = {}
 warrior = Character('warrior', 200, 15, 20)
 mage = Character('mage', 130, 30, 10)
 
 dict_of_characters[warrior.rase] = warrior
 dict_of_characters[mage.rase] = mage
+
+
+def start_game():
+    '''
+    User will chose if he want's to start the game or quit
+    before even trying.
+    '''
+
+    input_start = input('Type "start" to start the game or "exit" to quit:\n')
+    start = input_start.lower()
+
+    if start == 'start':
+        return
+    elif start == 'exit':
+        print('You could have at least try. Good bye!')
+        sys.exit()
+    elif start != 'start' or 'exit':
+        print('Don\'t understand.')
+        start_game()
 
 
 def character_choice():
@@ -35,11 +67,14 @@ def character_choice():
     '''
 
     while True:
-        user_input = input('Choose character. Warrior or mage?\n')
+        print('Choose a rase you want to play with:\n')
+        print(f'{warrior} \n')
+        print(f'{mage} \n')
+        user_input = input('Type warrior or mage?\n')
         user_choice = user_input.lower()
 
         if validate_input(user_choice):
-            print('You have chosen:\n' ,dict_of_characters[user_choice])
+            print('\n You have chosen:\n', dict_of_characters[user_choice])
             break
 
     return user_choice
@@ -60,24 +95,18 @@ def validate_input(choice):
     return True
 
 
-def start_game():
-    '''
-    User will chose if he want's to start the game or quit
-    before even trying.
-    '''
+def message(string):
+   
+    for i in string:
+       
+        # printing each character of the message
+        print(i, end='')
+         
+        # adding time delay of half second
+        time.sleep(0.5)
 
-    input_start = input('Type "start" to start the game or "exit" to quit:\n')
-    start = input_start.lower()
 
-    if start == 'start':
-        print('Let\' begin')
-        return
-    elif start == 'exit':
-        print('See you later')
-        sys.exit()
-    elif start != 'start' or 'exit':
-        print('Don\'understand')
-        start_game()
+message(MSG)
 
 
 def main():
@@ -86,6 +115,6 @@ def main():
 
 
 print('Welcome to role playing game!\n')
-
 main()
+
 
